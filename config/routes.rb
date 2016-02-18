65f1fa5d0f19ba73495a4e79a7dd30388f87d616
Harsheek Thanki <harsheekthanki@gmail.com>
2016-02-18 22:47:24 +0000

    Add course model and courses controller to app. Add simple_form form to allow Teachers to add a new course

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +59,5 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'static_pages#index'
+
 end
