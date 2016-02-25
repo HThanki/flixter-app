de4407ceee5c1c4346b2e51ba829a85165a832dd
Harsheek Thanki <harsheekthanki@gmail.com>
2016-02-25 00:46:26 +0000

    Add enrolled in course check method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+    enrolled_courses = []
+    enrollments.each do |enrollment|
+      enrolled_courses << enrollment.course
+    end
+
+    return enrolled_courses.include?(course)
+  end
+
 end
